a64a7710d63e75e8d504d486cabf78421ecae127
Khomenko Danil <dan4ikhom@gmail.com>
2023-02-09 20:05:36 +0700

    Фикс бага картинки

--- huntShop/settings.py
+++ huntShop/settings.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
-STATICFILES_DIR = [STATIC_DIR]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
